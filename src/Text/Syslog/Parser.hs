87a466ba350a0fc819e4d327c3e2c3f3202f1a68
Keith Duncan <keith.duncan@github.com>
2015-12-03 16:00:04 +1100

    Start modelling structured data

--- src/Text/Syslog/Parser.hs
+++ src/Text/Syslog/Parser.hs
@@ -27,9 +27,17 @@ data LogEntry = LogEntry { getPriority :: String
                          , getAppname :: String
                          , getProcessId :: String
                          , getMessageId :: String
-                         , getStructuredData :: [[String]]
+                         , getStructuredData :: [StructuredData]
                          , getMessage :: String
                          }
+
+data StructuredData = StructuredData { getId :: String
+                                     , getParams :: [Param]
+                                     }
+
+type Key = String
+type Value = String
+type Param = (Key, Value)
 
 parseSyslog :: String -> Either ParseError LogEntry
 parseSyslog = parse syslogLine "(unknown)"
@@ -51,12 +59,11 @@ nonZeroDigit = oneOf "123456789"
 version = liftM2 (:) nonZeroDigit (occurrences 0 2 digit)
 
 occurrences min' max' parser
+  | min' > max'  = error "you dun goofed"
   | min' == max' = count max' parser
-occurrences min' max' parser = try (count max' parser) <|> occurrences min' (max'-1) parser
+  | otherwise    = try (count max' parser) <|> occurrences min' (max'-1) parser
 
-timestamp =
- (nilvalue >> return "")
- <|> (mconcat <$> sequence [fullDate, string "T", fullTime])
+timestamp = (nilvalue >> return "") <|> (mconcat <$> sequence [fullDate, string "T", fullTime])
 
 nilvalue = string "-"
 
@@ -64,12 +71,7 @@ fullDate = mconcat <$> sequence [count 4 digit, string "-", count 2 digit, strin
 
 fullTime = liftM2 (++) partialTime timeOffset
 
-partialTime = do
- time <- time
- sep2 <- string ":"
- second <- second
- frac <- optionMaybe (liftM2 (++) (string ".") (occurrences 1 6 digit))
- return $ time ++ sep2 ++ second ++ fromMaybe "" frac
+partialTime = mconcat <$> sequence [time, string ":", second, fromMaybe "" <$> optionMaybe (liftM2 (++) (string ".") (occurrences 1 6 digit))]
 
 timeHour = count 2 digit
 timeMinute = count 2 digit
@@ -89,13 +91,16 @@ msgid = nilvalue <|> occurrences 1 32 printascii
 
 structuredData = (nilvalue >> return []) <|> many1 sdElement
 
-sdElement = between (char '[') (char ']') (liftM2 (:) sdId (many (space >> sdParam)))
+sdElement = between (char '[') (char ']') (liftM2 StructuredData sdId (many (space >> sdParam)))
 
 sdId = sdName
 
 sdName = occurrences 1 32 (oneOf (filter (`notElem` "= ]\"") printasciiSet))
 
-sdParam = mconcat <$> sequence [paramName, string "=", string "\"", paramValue, string "\""]
+sdParam = liftM2 (,) paramName (string "=" >> quoted paramValue)
+
+quoted = between doubleQuote doubleQuote
+doubleQuote = char '"'
 
 paramName = sdName
 paramValue = string ""
