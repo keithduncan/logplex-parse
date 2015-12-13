0394c98ab860b02a4786ee9d52b4cef8020acf22
Keith Duncan <keith.duncan@github.com>
2015-12-13 17:19:47 +1100

    Better matcher

--- spec/Text/Logplex/ParserSpec.hs
+++ spec/Text/Logplex/ParserSpec.hs
@@ -5,11 +5,9 @@ import Text.Syslog.Parser
 import Data.Time.Clock.POSIX
 import Test.Hspec
 
-fromRight (Right x) = x
-
 spec = do
   describe "Logplex Parser" $ do
     it "should parse frames" $ do
-      let logEntries = fromRight $ parseLogplex "110 <10>123 2015-12-03T23:12:17+11:00 keiths-macbook-pro.local logplex-parse 420 - [foo bar=\"\\\\\\\"baz\"] troll=lolol110 <10>123 2015-12-03T23:12:17+11:00 keiths-macbook-pro.local logplex-parse 420 - [foo bar=\"\\\\\\\"baz\"] troll=lolol"
+      let logEntries = parseLogplex "110 <10>123 2015-12-03T23:12:17+11:00 keiths-macbook-pro.local logplex-parse 420 - [foo bar=\"\\\\\\\"baz\"] troll=lolol110 <10>123 2015-12-03T23:12:17+11:00 keiths-macbook-pro.local logplex-parse 420 - [foo bar=\"\\\\\\\"baz\"] troll=lolol"
       let entry = LogEntry "10" "123" (posixSecondsToUTCTime 1449144737) "keiths-macbook-pro.local" "logplex-parse" "420" "-" [StructuredData "foo" [("bar", "\\\"baz")]] (Just "troll=lolol")
-      logEntries `shouldBe` [entry, entry]
+      logEntries `shouldBe` Right [entry, entry]
