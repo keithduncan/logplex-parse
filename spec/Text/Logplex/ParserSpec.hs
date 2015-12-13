6c84be4d680c2079df2aab0bbace72f5602d2825
Keith Duncan <keith.duncan@github.com>
2015-12-13 17:32:56 +1100

    Make the seconds fractional

--- spec/Text/Logplex/ParserSpec.hs
+++ spec/Text/Logplex/ParserSpec.hs
@@ -17,5 +17,5 @@ spec = do
       -- because STRUCTURED-DATA is defined as a NILVALUE or at least one
       -- SD-ELEMENT neither of which are present here.
       let logEntries = parseLogplex "277 <158>1 2015-12-13T06:02:17.590861+00:00 host heroku router - at=error code=H10 desc=\"App crashed\" method=POST path=\"/crash\" host=ancient-savannah-2923.herokuapp.com request_id=fdcf3bf3-97d3-48a5-8eff-3b28789e36ee fwd=\"120.148.233.212\" dyno= connect= service= status=503 bytes=\n"
-      let entry = LogEntry "158" "1" (posixSecondsToUTCTime 1449986537) "host" "heroku" "router" "-" [] (Just "at=error code=H10 desc=\"App crashed\" method=POST path=\"/crash\" host=ancient-savannah-2923.herokuapp.com request_id=fdcf3bf3-97d3-48a5-8eff-3b28789e36ee fwd=\"120.148.233.212\" dyno= connect= service= status=503 bytes=\n")
+      let entry = LogEntry "158" "1" (posixSecondsToUTCTime 1449986537.590861) "host" "heroku" "router" "-" [] (Just "at=error code=H10 desc=\"App crashed\" method=POST path=\"/crash\" host=ancient-savannah-2923.herokuapp.com request_id=fdcf3bf3-97d3-48a5-8eff-3b28789e36ee fwd=\"120.148.233.212\" dyno= connect= service= status=503 bytes=\n")
       logEntries `shouldBe` Right [entry]
