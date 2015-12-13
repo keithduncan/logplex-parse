5e0ae7ead50c5feb892ea565fec5b9dfa13a30a6
Keith Duncan <keith.duncan@github.com>
2015-12-13 17:19:37 +1100

    Better matcher

--- spec/Text/Syslog/ParserSpec.hs
+++ spec/Text/Syslog/ParserSpec.hs
@@ -4,10 +4,8 @@ import Text.Syslog.Parser
 import Data.Time.Clock.POSIX
 import Test.Hspec
 
-fromRight (Right x) = x
-
 spec = do
   describe "Syslog Parser" $ do
     it "should parse lines" $ do
-      let logEntry = fromRight $ parseSyslog "<10>123 2015-12-03T23:12:17+11:00 keiths-macbook-pro.local logplex-parse 420 - [foo bar=\"\\\\\\\"baz\"] troll=lolol"
-      logEntry `shouldBe` LogEntry "10" "123" (posixSecondsToUTCTime 1449144737) "keiths-macbook-pro.local" "logplex-parse" "420" "-" [StructuredData "foo" [("bar", "\\\"baz")]] (Just "troll=lolol")
+      let logEntry = parseSyslog "<10>123 2015-12-03T23:12:17+11:00 keiths-macbook-pro.local logplex-parse 420 - [foo bar=\"\\\\\\\"baz\"] troll=lolol"
+      logEntry `shouldBe` Right (LogEntry "10" "123" (posixSecondsToUTCTime 1449144737) "keiths-macbook-pro.local" "logplex-parse" "420" "-" [StructuredData "foo" [("bar", "\\\"baz")]] (Just "troll=lolol"))
